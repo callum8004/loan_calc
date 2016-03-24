2805c07ecaa6a6298d97c810bfc1c1488582800e
Joel Bradshaw <cincodenada@gmail.com>
2016-03-23 22:17:03 -0700

    Whitespace

--- main.js
+++ main.js
@@ -3,93 +3,93 @@ var ReactDOM = require('react-dom');
 var LocalStorageMixin = require('react-localstorage');
 
 var Loan = React.createClass({
-    render: function() {
-      return (
-        <tr>
-          <td>{this.props.balance}</td>
-          <td>{this.props.interest_rate}</td>
-          <td>{this.props.min_payment}</td>
-        </tr>
-      )
-    }
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.balance}</td>
+        <td>{this.props.interest_rate}</td>
+        <td>{this.props.min_payment}</td>
+      </tr>
+    )
+  }
 })
 
 var LoanTable = React.createClass({
-    render: function() {
-      var loanNodes = this.props.loans.map(function(loan) {
-        return (
-          <Loan
-            balance={loan.balance}
-            interest_rate={loan.interest_rate}
-            min_payment={loan.min_payment}
-          ></Loan>
-        )
-      })
+  render: function() {
+    var loanNodes = this.props.loans.map(function(loan) {
       return (
-        <table className="table loan_table">
-          <thead>
-            <tr>
-              <th>Balance</th>
-              <th>Interest Rate</th>
-              <th>Minimum Payment</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loanNodes}
-          </tbody>
-        </table>
+        <Loan
+          balance={loan.balance}
+          interest_rate={loan.interest_rate}
+          min_payment={loan.min_payment}
+        ></Loan>
       )
-    }
+    })
+    return (
+      <table className="table loan_table">
+        <thead>
+          <tr>
+            <th>Balance</th>
+            <th>Interest Rate</th>
+            <th>Minimum Payment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loanNodes}
+        </tbody>
+      </table>
+    )
+  }
 })
 
 var CSVInput = React.createClass({
-    handleCSVChange: function(e) {
-      var lines = e.target.value.split("\n")
-      var loans = []
-      lines.forEach(function(line) {
-        vals = line.split(',')
-        loans.push({
-          balance: vals[0],
-          interest_rate: vals[1],
-          min_payment: vals[2],
-        })
+  handleCSVChange: function(e) {
+    var lines = e.target.value.split("\n")
+    var loans = []
+    lines.forEach(function(line) {
+      vals = line.split(',')
+      loans.push({
+        balance: vals[0],
+        interest_rate: vals[1],
+        min_payment: vals[2],
       })
+    })
 
-      this.props.onLoanData(loans)
-    },
-    render: function() {
-      return (
-        <textarea placeholder="Paste CSV data here" onChange={this.handleCSVChange}></textarea>
-      )
-    }
+    this.props.onLoanData(loans)
+  },
+  render: function() {
+    return (
+      <textarea placeholder="Paste CSV data here" onChange={this.handleCSVChange}></textarea>
+    )
+  }
 })
 
 var InputtableLoanList = React.createClass({
-    mixins: [LocalStorageMixin],
+  mixins: [LocalStorageMixin],
 
-    getInitialState: function() {
-      return {
-        loans: []
-      }
-    },
-    componentDidMount: function() {
-      if(stored_state = localStorage.getItem("loan_state")) {
-        if(parsed_state = JSON.parse(localStorage.getItem("loan_state"))) {
-          this.setState(parsed_state)
-        }
-      }
-    },
-    setLoanData: function(loan_data) {
-      this.setState({loans: loan_data})
-    },
-    render: function() {
-      return (
-        <div>
-          <CSVInput onLoanData={this.setLoanData}/>
-          <LoanTable loans={this.state.loans}/>
-        </div>
-      )
+  getInitialState: function() {
+    return {
+      loans: []
     }
+  },
+  componentDidMount: function() {
+    if(stored_state = localStorage.getItem("loan_state")) {
+      if(parsed_state = JSON.parse(localStorage.getItem("loan_state"))) {
+        this.setState(parsed_state)
+      }
+    }
+  },
+  setLoanData: function(loan_data) {
+    this.setState({loans: loan_data})
+  },
+  render: function() {
+    return (
+      <div>
+        <CSVInput onLoanData={this.setLoanData}/>
+        <LoanTable loans={this.state.loans}/>
+      </div>
+    )
+  }
 })
 
 ReactDOM.render(
